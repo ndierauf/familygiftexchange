25bb5b5b1a2d2848d695e525c2c93deb1997fb4f
ndierauf <ndierauf@outlook.com>
2020-10-12 18:13:39 -0700

    Merge from Part2Branch

--- src/main/java/com/dierauf/rachio/familygiftexchange/model/GiftExchangeGenerator.java
+++ src/main/java/com/dierauf/rachio/familygiftexchange/model/GiftExchangeGenerator.java
@@ -1,6 +1,5 @@
 package com.dierauf.rachio.familygiftexchange.model;
 
-<<<<<<< HEAD
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -16,24 +15,11 @@ import org.apache.logging.log4j.Logger;
 /**
  *
  * @author Nicholas Dierauf
-=======
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-
-/**
- *
- * @author nicho
->>>>>>> 9e66c6f803d510589272b45cf206c8a2a9fb42df
  * A Singleton class for generating family gift exchange pairs.
  * Can be considered a 'Service'.
  */
 public class GiftExchangeGenerator {
 
-<<<<<<< HEAD
 	static final String INSTRUCTIONS = "First arguement must be a number indicating the number of family members per family unit, followed by a list of family members. "
 			+ "Example: \njava -jar FamilyGiftExchange.jar 2 Nick Trevor Amy Sam";
 	static final String ERROR_MESSAGE_FAMILY_MEMBER_GIVING_TO_ANOTHER_FAMILY_MEMBER = "Family member giving to another family member. ";
@@ -58,18 +44,12 @@ public class GiftExchangeGenerator {
 	private static final GiftExchangeGenerator INSTANCE = new GiftExchangeGenerator(); // Singleton created on class load.
 
 
-=======
-	static final String ERROR_MESSAGE_MUST_HAVE_AT_LEAST_TWO_FAMILY_MEMBERS = "Must have at least two family members to generate gift exchange. ";
-	private static final GiftExchangeGenerator INSTANCE = new GiftExchangeGenerator(); // Singleton created on class load.
-
->>>>>>> 9e66c6f803d510589272b45cf206c8a2a9fb42df
 	// Assures Singleton.
 	private GiftExchangeGenerator() {};
 	public static GiftExchangeGenerator instance() {
 		return INSTANCE;
 	}
 
-<<<<<<< HEAD
 	/**
 	 * @param args
 	 * @throws Exception
@@ -367,52 +347,3 @@ public class GiftExchangeGenerator {
 
 }
 
-=======
-	public Map<Integer, Integer> generateGiftExchanges(Set<Integer> familyMemberIds) throws Exception {
-
-		// Parameter validation.
-		if (familyMemberIds == null || familyMemberIds.size() < 2) {
-			throw new Exception(ERROR_MESSAGE_MUST_HAVE_AT_LEAST_TWO_FAMILY_MEMBERS);
-		}
-
-		Set<Integer> giverIds = familyMemberIds;
-
-		Random random = new Random();
-		List<Integer> receiverIds = new ArrayList<>(giverIds);
-		Map<Integer, Integer> giverReceiverMap = new HashMap<>();
-		for (Integer giverId : giverIds) {
-			// Temporarily remove giverId from receiverIds so that giver cannot be matched to itself.
-			boolean isTemporarilyRemoved = receiverIds.remove(giverId);
-			// receiverIds is now guaranteed not to have giverId.
- 			Integer receiverId = (receiverIds.isEmpty()) ?
- 				// If receiverIds.isEmpty() then we are on our last giverId and there are no receiverIds to choose from.
- 				// Need to 'swap' a random receiverId from a previous selection with the current giverId.
-				this.swapReceiverIdWithSomeoneElse(giverId, giverReceiverMap, random) :
-				this.retrieveReceiverId(receiverIds, random);
-			giverReceiverMap.put(giverId, receiverId);
-			// Replace giverId from receiverIds if it had been temporarily removed.
-			if (isTemporarilyRemoved) {
-				receiverIds.add(giverId);
-			}
-		}
-
-		return giverReceiverMap;
-	}
-
-	private Integer retrieveReceiverId(List<Integer> receiverIds, Random random) {
-		int index = random.nextInt(receiverIds.size());
-		Integer receiverId = receiverIds.remove(index);
-		return receiverId;
-	}
-
-	private Integer swapReceiverIdWithSomeoneElse(Integer giverId, Map<Integer, Integer> giverReceiverMap, Random random) {
-		// Select random index from keySet:
-		Integer[] currentGiverIds = giverReceiverMap.keySet().toArray(new Integer[giverReceiverMap.size()]);
-		int randomGiverId = random.nextInt(currentGiverIds.length);
-		int newReceiverId = giverReceiverMap.get(randomGiverId);
-		giverReceiverMap.put(randomGiverId, giverId);
-		return newReceiverId;
-	}
-
-}
->>>>>>> 9e66c6f803d510589272b45cf206c8a2a9fb42df
